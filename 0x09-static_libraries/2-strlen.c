5e5c4de8aed3e5e687402f510aa9407c6f725efd
hesbon-soft <birunduhezbon@gmail.com>
2023-04-09 04:53:45 -0700

    more files

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *@s: string
+ * Return: return count
+ */
+
+int _strlen(char *s)
+{
+	int x;
+	int count = 0;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		count++;
+	}
+	return (count);
+}
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - prints lowercase character
+ *
+ *@c: parameter to be printed
+ * Return: 1 if lowercase else 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
