abae59b345933a91e6047eb66444ea09901e397d
hesbon-soft <birunduhezbon@gmail.com>
2023-04-10 01:48:15 -0700

    a program that prints a new name followed by anew line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argument count.
+ * @argv: array count.
+ * Return: Always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int main(int argc, char *argv[]);
+int _putchar(char c);
+int _atoi(char *s);
+
+
+
+#endif
