de8eacf0341374ba1ae4f9734adbaea9f0ceaf7c
hesbon-soft <birunduhezbon@gmail.com>
2023-04-10 05:03:57 -0700

    a progam that multplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,18 +9,19 @@
  * Return: 0 (Success), 1 (Error)
  */
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
 	int x, y, z;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return 1;
+		return (1);
 	}
 
 	x = atoi(argv[1]);
 	y = atoi(argv[2]);
 	z = x * y;
 	printf("%d\n", z);
-	return 0;
+	return (0);
 }
