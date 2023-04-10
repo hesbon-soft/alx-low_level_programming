7d1dfe9df87b464edcaa6e1efa59e3e26e1e7b4c
hesbon-soft <birunduhezbon@gmail.com>
2023-04-10 04:59:11 -0700

    a progam that multplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+
+int main(int argc, char *argv[]) 
+{
+	int x, y, z;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return 1;
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	z = x * y;
+	printf("%d\n", z);
+	return 0;
+}
